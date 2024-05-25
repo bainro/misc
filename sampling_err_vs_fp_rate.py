b9b542526186c5cd3695cfbb1514e86df700036d
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-05-25 01:04:26 -0700

    Update sampling_err_vs_fp_rate.py

--- sampling_err_vs_fp_rate.py
+++ sampling_err_vs_fp_rate.py
@@ -1,18 +1,18 @@
+import random
 import numpy as np  
 from scipy import stats  
 
-N = 30            # sample size
+N = 100 # sample size
 mu1 = mu2 = 10
-FPs = 0           # false positives
-num_tests = 1000
+num_tests = 10000
 
+FPs = 0 # false positives
 for _ in range(num_tests):
   # Gaussian distributed data with mean muX and var = 1  
   x = np.random.randn(N) + mu1
   y = np.random.randn(N) + mu2
   
   _t_stat, p_val = stats.ttest_ind(x, y)
-  # print(f'p-value: {p_val}')
   if p_val < 0.05:
     FPs += 1
 
@@ -20,14 +20,41 @@ print(f'False Positive Rate: {FPs / num_tests:.2f}')
 
 FPs = 0
 for _ in range(num_tests):
-  # Gaussian distributed data with mean muX and var = 1  
   x = np.random.randn(N) + mu1
   y = np.random.randn(N) + mu2
   
-  # could x and y have different error distributions?
+  x = [random.random() * x + 5 for x in x]
+  y = [random.random() * y + 5 for y in y]
   
   _t_stat, p_val = stats.ttest_ind(x, y)
-  # print(f'p-value: {p_val}')
+  if p_val < 0.05:
+    FPs += 1
+
+print(f'False Positive Rate: {FPs / num_tests:.2f}')
+
+FPs = 0
+for _ in range(num_tests):
+  x = np.random.randn(N) + mu1
+  y = np.random.randn(N) + mu2
+  
+  x = [random.random() ** x + 5 for x in x]
+  y = [random.random() ** y + 5 for y in y]
+  
+  _t_stat, p_val = stats.ttest_ind(x, y)
+  if p_val < 0.05:
+    FPs += 1
+
+print(f'False Positive Rate: {FPs / num_tests:.2f}')
+
+FPs = 0
+for _ in range(num_tests):
+  x = np.random.randn(N) + mu1
+  y = np.random.randn(N) + mu2
+  
+  x = [x ** (random.random() * 2) + 5 for x in x]
+  y = [y ** (random.random() * 2) + 5 for y in y]
+  
+  _t_stat, p_val = stats.ttest_ind(x, y)
   if p_val < 0.05:
     FPs += 1
 
