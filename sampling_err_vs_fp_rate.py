4d27217aa6dec9d451716320097674b2f3382e88
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-05-25 00:31:57 -0700

    Update sampling_err_vs_fp_rate.py

--- sampling_err_vs_fp_rate.py
+++ sampling_err_vs_fp_rate.py
@@ -1,15 +1,20 @@
 import numpy as np  
 from scipy import stats  
 
-# Sample Sizes
-N = 30
+N = 30            # sample size
 mu1 = mu2 = 10
+FPs = 0           # false positives
+num_tests = 1000
 
-# Gaussian distributed data with mean = 10.5 and var = 1  
-x = np.random.randn(N) + mu1
-y = np.random.randn(N) + mu2
+for _ in range(num_tests):
+  # Gaussian distributed data with mean = 10.5 and var = 1  
+  x = np.random.randn(N) + mu1
+  y = np.random.randn(N) + mu2
+  
+  ## Using the internal function from SciPy Package  
+  _t_stat, p_val = stats.ttest_ind(x, y)
+  print(f'p-value: {p_val}')
+  if p_val < 0.05:
+    FPs += 1
 
-## Using the internal function from SciPy Package  
-t_stat, p_val = stats.ttest_ind(x, y)
-print("t-statistic = " + str(t_stat))
-print("p-value = " + str(p_val))
+print(f'False Positive Rate: {FPs / num_tests:.2f}')
